e577a75d1d7738423d08c48eb25370ce8ae65109
NdegwaJulius <juliusndegwa2@gmail.com>
2022-08-02 08:11:27 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
+
 
